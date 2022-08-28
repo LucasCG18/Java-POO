20270c94f14132dd959eee03e4ec21a01d8f9eac
LucasCG18 <89227510+LucasCG18@users.noreply.github.com>
2022-08-28 00:45:40 -0300

    polimento

--- projeto-3-bi/ManipularCaracteres.java
+++ projeto-3-bi/ManipularCaracteres.java
@@ -1,9 +1,5 @@
 public class ManipularCaracteres {
     ManipularCaracteres(Caracteres numero){
-        if (numero.getNumero() != 0 && numero.getNumero() != 1) {
-            numero.setnumero_extenso(numero.getnumero_extenso().substring(0,1).toUpperCase() + numero.getnumero_extenso().substring(1, numero.getnumero_extenso().length()));
-        }else{
-            numero.setnumero_extenso(numero.getnumero_extenso().substring(0,1).toUpperCase() + numero.getnumero_extenso().substring(1, numero.getnumero_extenso().length()));
-        }
+        numero.setnumero_extenso(numero.getnumero_extenso().substring(0,1).toUpperCase() + numero.getnumero_extenso().substring(1, numero.getnumero_extenso().length()));
     }
 }
