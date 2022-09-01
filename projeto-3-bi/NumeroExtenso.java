a5695a6e02f8a65bd27524a65c481bb96cac34f7
LucasCG18 <89227510+LucasCG18@users.noreply.github.com>
2022-09-01 13:39:47 -0300

    Update NumeroExtenso.java

--- projeto-3-bi/NumeroExtenso.java
+++ projeto-3-bi/NumeroExtenso.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class NumeroExtenso {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Caracteres numero = new Caracteres();
-        numero.setNumero(Integer.parseInt(in.nextLine()));
+        numero.setNumero(Integer.parseInt(scanner.nextLine()));
         Unidade num = new Unidade(numero);
         ManipularCaracteres manipularCaracteres = new ManipularCaracteres(numero);
         System.out.println(numero.getnumero_extenso());
